b4e23bfc63f2a07f0cd4b3b6a74145f17aa59f24
sak <u695902k@gmail.com>
2022-01-15 09:05:27 +0900

    finish section3

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,8 @@
-resource "aws_instance" "example"{
-    ammi    = "ami-0c3fd0f5d33134a76"
-    instance_type ="t3.micro"
+module "web_server"{
+    source ="./http_server"
+    instance_type = "t3.micro"
+}
+
+output "public_dns"{
+    value = module.web_server.public_dns
 }
