3341f6c5c2c8b149c926dfb71e4e49bb354ed596
sak <u695902k@gmail.com>
2022-01-15 13:10:41 +0900

    main.tf devided into s3.tf and network.tf

---
+++ example/s3.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "private" {
+    bucket = "private-pragmatic-terraform20220115"
+
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration{
+        rule{
+            apply_server_side_encryption_by_default{
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "private"{
+    bucket = aws_s3_bucket.private.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "public" {
+    bucket = "public-pragmatic-terraform-20220115"
+    acl = "public-read"
+
+    cors_rule {
+        allowed_origins = ["https://example.com"]
+        allowed_methods = ["GET"]
+        allowed_headers = ["*"]
+        max_age_seconds = 3000
+    }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+    bucket = "alb-log-pragmatic-terraform20220115"
+
+    lifecycle_rule {
+        enabled = true
+
+        expiration {
+            days = "180"
+        }
+    }
+}
