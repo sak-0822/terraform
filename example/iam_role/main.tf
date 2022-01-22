594c7e58efb1d6c8210a3cbe94e7ceb20f651c20
sak <u695902k@gmail.com>
2022-01-22 15:44:48 +0900

    modulize aws_iam_policy

--- example/iam_role/main.tf
+++ example/iam_role/main.tf
@@ -18,6 +18,20 @@ data "aws_iam_policy_document" "assume_role"{
     }
 }
 
+resource "aws_iam_policy" "default"{
+    name = var.name
+    policy = var.policy
+}
+
+resource "aws_iam_role_policy_attachment" "default"{
+    role = aws_iam_role.default.name
+    policy_arn = aws_iam_policy.default.arn
+}
+
+output "iam_role_arn" {
+    value = aws_iam_role.default.arn
+}
+
 output "iam_role_name" {
     value = aws_iam_role.default.name
 }
